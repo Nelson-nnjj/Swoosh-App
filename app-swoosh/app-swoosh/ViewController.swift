5f518ebe108a27f0ae5cff71194696619c276dfe
Nelson Ng Jia Jun <nelsonnnjj@gmail.com>
2018-11-30 10:26:55 +0000

    second commit

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Nelson  Ng Jia Jun on 30/11/2018.
+//  Copyright © 2018 Nelson Ng Jia Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ app-swoosh/app-swoosh/Views/Border Button.swift
@@ -0,0 +1,21 @@
+//
+//  Border Button.swift
+//  app-swoosh
+//
+//  Created by Nelson  Ng Jia Jun on 30/11/2018.
+//  Copyright © 2018 Nelson Ng Jia Jun. All rights reserved.
+//
+
+import UIKit
+
+class Border_Button: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
